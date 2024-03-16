5c4910d665c1bcb9c9ff0ceb1a909562ab2de39c
Snay1 <89585275+Snay1@users.noreply.github.com>
2024-03-16 12:50:24 +0300

    routing update

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
     import { useUserStore } from "~/store";
-    import { Button, Input } from '~/components';
+    import { Button, Input, Logo } from '~/components';
 
     const router = useRouter();
 
@@ -40,9 +40,14 @@
 
 </script>
 <template>
-    <div class="w-full h-full flex items-center justify-center">
+    <div class="w-full h-full flex items-center justify-center flex-col">
+        <NuxtLink to="/" class="flex items-center justify-center mb-[20px] text-xl hover:text-[#3FBAFF] transition-all">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>
+            На главную
+        </NuxtLink>
         <form class="flex flex-col gap-4" @submit.prevent="loginHandler">
-            <h1 class="text-center text-2xl uppercase font-bold text-slate-700">Мультиадминка</h1>
+            <Logo />
+            <p class="text-center text-lg">войти.</p>
             <Input 
                 type="email"
                 :value="email"
@@ -55,9 +60,15 @@
                 placeholder="Пароль"
                 @input="(e) => password = e.target.value"
             />
-            <Button type="submit">
+            <Button type="submit" buttonClass="!bg-[#3FBAFF]">
                 Войти
             </Button>
+            <div>
+                <p class="w-full text-center mt-[20px] mb-[10px]">Нет аккаунта?</p>
+                <Button link="/register">
+                    регистрация.
+                </Button>
+            </div>
         </form>
     </div>
 </template>
--- pages/register.vue
+++ pages/register.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
     import { useUserStore } from "~/store";
-    import { Button, Input } from '~/components';
+    import { Button, Input, Logo } from '~/components';
 
     const router = useRouter();
 
@@ -45,10 +45,14 @@
 
 </script>
 <template>
-    <div class="w-full h-full flex items-center justify-center">
+    <div class="w-full h-full flex items-center justify-center flex-col">
+        <NuxtLink to="/" class="flex items-center justify-center mb-[20px] text-xl hover:text-[#3FBAFF] transition-all">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>
+            На главную
+        </NuxtLink>
         <form class="flex flex-col gap-4" @submit.prevent="loginHandler">
-            <p class="text-center text-xl">регистрация.</p>
-            <h1 class="text-center text-2xl uppercase font-bold text-slate-700">Мультиадминка</h1>
+            <Logo />
+            <p class="text-center text-lg">регистрация.</p>
             <Input 
                 type="email"
                 :value="email"
@@ -70,6 +74,12 @@
             <Button type="submit">
                 Зарегистрироваться
             </Button>
+            <div>
+                <p class="w-full text-center mt-[20px] mb-[10px]">Уже есть аккаунт?</p>
+                <Button link="/login" buttonClass="!bg-[#3FBAFF]">
+                    войти.
+                </Button>
+            </div>
         </form>
     </div>
 </template>
