a5f4a2bd324a94d06eac0c0a51a3357f19721107
Snay1 <89585275+Snay1@users.noreply.github.com>
2024-03-26 19:31:58 +0300

    footer added

--- components/Footer.vue
+++ components/Footer.vue
@@ -7,10 +7,20 @@
 </script>
 <template>
     <footer class="py-[20px] border-t border-l-stone-900 border-solid bg-[#292929] text-white">
-        <Container>
+        <Container class="flex flex-col md:flex-row md:items-center justify-between gap-[20px]">
             <p class="text-xl font-bold uppercase">
                 Все права защищены © {{ year }}
             </p>
+            <ul>
+                <li>Самозанятый Тропин Степан Васильевич</li>
+                <li>ИНН 291401505660</li>
+                <li>Почта для связи: <a href="mailto:marketplacehelpercall@gmail.com" class="underline">marketplacehelpercall@gmail.com</a></li>
+                <li>
+                    <NuxtLink to="/privacy" class="underline">
+                        Политика конфиденциальности
+                    </NuxtLink>
+                </li>
+            </ul>
         </Container>
     </footer>
 </template>
